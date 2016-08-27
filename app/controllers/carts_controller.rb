7e753a39bcdb285c51395ed56dbe4273fbc5e4ac
yusukeagata <yusuke.agata1013@gmail.com>
2016-08-27 15:21:17 +0000

    taskF

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -70,7 +70,7 @@ class CartsController < ApplicationController
     @cart.destroy
     session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to store_url,notice: 'カートは現在空です' }
+      format.html { redirect_to store_url }
       format.json { head :no_content }
     end
   end
