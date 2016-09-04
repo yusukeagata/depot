14f3adcf2b80b6b00bbbe897b110fcc4ea4058fc
yusukeagata <yusuke.agata1013@gmail.com>
2016-09-04 11:15:35 +0000

    master02

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_filter :authorize
+  #before_filter :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :user_params, only: [:create, :edit, :update]
 
   # GET /users
   # GET /users.json
@@ -74,6 +75,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :password_digest)
+     params.require(:user).permit(:name, :password, :password_confirmation, :ksuid)
     end
 end
