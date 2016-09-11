045bc56a324d935f945e80c835d3e4acc6fdb6c4
yusukeagata <yusuke.agata1013@gmail.com>
2016-09-11 07:12:15 +0000

    paginate

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
   skip_before_filter :authorize
   def index
-    @products = Product.order(:title)
+    @products = Product.order(:title).paginate :page=>params[:page], per_page: 20
     @cart = current_cart
   end
 end
